5d3915eb09237f7c9fe5f9bf8346ad7650b5d43c
Marcin Stencel <marc.stencel@gmail.com>
2023-11-04 14:17:17 +0100

    Modify env variables

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.FB_API_KEY,
-  authDomain: import.meta.env.FB_AUTH_DOMAIN,
-  databaseURL: import.meta.env.FB_DATA_BASE_URL,
-  projectId: import.meta.env.FB_PROJECT_ID,
-  storageBucket: import.meta.env.FB_STORAGE_BUCKE,
-  messagingSenderId: import.meta.env.FB_MESSAGING_SENDER_ID,
-  appId: import.meta.env.FB_APP_ID,
+  apiKey: import.meta.env.VITE_FB_API_KEY,
+  authDomain: import.meta.env.VITE_FB_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FB_DATA_BASE_URL,
+  projectId: import.meta.env.VITE_FB_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FB_STORAGE_BUCKE,
+  messagingSenderId: import.meta.env.VITE_FB_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FB_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
